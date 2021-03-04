79d9bdb1edc5a6956c8c2829ef7aaf1e9b29b1e9
Oliver Sierra Pac <sierra.oe@gmail.com>
2021-03-04 17:00:09 -0600

    clase juego creada

--- src/Ejecutable.java
+++ src/Ejecutable.java
@@ -4,13 +4,8 @@
 public class Ejecutable {
 
     public static void main(String args[]){
-       // asdf
-       System.out.println("");
-       //Personaje per = new Personaje(vida, armadura, ataque, nombre, esHeroe)
-        Elfo elf = new Elfo("Oliver");
-        elf.saludar();
-        Hobbit hob = new Hobbit("frodo");
-        hob.saludar();
+       juego tmp = new juego();
+       tmp.jugar();
 
 
     }
--- src/juego.java
+++ src/juego.java
@@ -1,3 +1,25 @@
 public class juego {
+
+    private Bestias ejercitoBestias[];
+    private Heroes ejercitoHeroes[];
+
+    private static int Cantidad = 5;
     
+    public juego(){
+        ejercitoBestias = new Bestias[Cantidad];
+        ejercitoHeroes = new Heroes[Cantidad];
+
+    }
+
+    public void jugar(){
+        System.out.println("Bienvenido al juego del señor de los anillos: \n\n");
+    }
+
+    public void inicializarHeroes(){
+        ejercitoHeroes[0]= new Elfo("legolas");
+        ejercitoHeroes[1]= new Hobbit("frodo");
+        ejercitoHeroes[2] = new Humano("Aragon");
+        ejercitoHeroes[3]= new Elfo("legolas2");
+        ejercitoHeroes[4]= new Hobbit("frodo3");
+    }
 }
